4ce01bd92ad00cb2c9bb9255edd52175ad1a2797
pka5667 <pka5667@gmail.com>
2021-05-01 23:00:36 +0530

    url for callback changes

--- shop/views.py
+++ shop/views.py
@@ -140,7 +140,7 @@ def checkout(request):
                 'INDUSTRY_TYPE_ID': 'Retail',
                 'WEBSITE': 'WEBSTAGING',
                 'CHANNEL_ID': 'WEB',
-                'CALLBACK_URL': "http://127.0.0.1:8000/shop/handlerequest"
+                'CALLBACK_URL': "http://pka5667-my-cart.herokuapp.com/shop/handlerequest"
             }
             param_dict['CHECKSUMHASH'] = checkSum.generate_checksum(param_dict, MERCHANT_KEY)
             return render(request, "shop/paytm.html", {'param_dict': param_dict})
